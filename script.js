e04e0f978bc7c26fb672f46a9af8bde81c228a61
PhilvanVeel <92588384+PhilvanVeel@users.noreply.github.com>
2022-10-04 22:57:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ const onProgress = (event) => {
   }
 };
 document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+document.addEventListener('wheel',function (event){
+  //only vertical scroll
+  if (event.deltaY > 0)
+  {
+    event.preventDefault();
+    smoothScroll(document.documentElement,100,1000)
+  }
+})
+function smoothScroll (domElement,pixel,delay)
+{
+  const intervalToRepeat = 25;
+  const step = (intervalToRepeat * pixel) / delay;
+  if ( step < pixel)
+  {
+    domElement.scrollTop += step;
+    setTimeout(function (){
+      smoothScroll(domElement,pixel - step,delay)
+    },intervalToRepeat);
+  }
+  
+  
+}
